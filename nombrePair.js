d007c24f527c95f5880d3743e7c6c9e8d1f9663f
Charles-MAD <charlesdang00@gmail.com>
2024-01-11 21:47:13 +0200

    changement de la variable n en nuber

--- nombrePair.js
+++ nombrePair.js
@@ -4,12 +4,12 @@
 
 // Utilisons la boucle for pour afficher les nombres pairs de 2 à 10
 // Utilisons la boucle for pour afficher les nombres impairs de 1 à 10
-let n = 1;
+let number = 1;
 for ( n; n <= 10; n++) {
-  if ( n % 2 !== 0 ) {
-    console.log( `${n} est un nombre impair`);
+  if ( number % 2 !== 0 ) {
+    console.log( `${number} est un nombre impair`);
   } else {
-    console.log( `${n} est un nombre pair` );
+    console.log( `${number} est un nombre pair` );
   }
 }
 console.log('Merci pour toutes vos reponses correctes')
