a17212e44ee582ef69f91fd182e5dbec4d74d2d5
Charles-MAD <charlesdang00@gmail.com>
2024-01-17 00:31:04 +0200

    Modification du code dans le fichier alphabetDonne.js

---
+++ alphabetDonne.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+let result = add(2,3);
+console.log(result);
+
+ console.log('');
+ 
+function mult(c, d) {
+    return c * d;
+}
+let resultat = mult(2,3);
+console.log(resultat);
