4f784c21623ffb7d1f581eeca3aa9e45ae053167
Charles-MAD <charlesdang00@gmail.com>
2024-01-16 11:16:07 +0200

    Modification du fichier nombrePremier.js

--- nombrePremier.js
+++ nombrePremier.js
@@ -6,5 +6,5 @@ for (let i = 2; i < 20; i++) { // Pour chaque i...
    for (let j = 2; j < i; j++) { // Cherche un diviseur
       if ( i % j == 0) continue nextPrime; // pas un premier, on passe au prochain i
    }
-   alert(`${i} est un nombre premier`); // un nombre premier
+   alert(`${i} est un nombre premier`); // C'est un nombre premier
 }
