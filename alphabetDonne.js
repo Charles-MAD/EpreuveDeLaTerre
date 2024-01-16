3789f3bbd7ca4863d4b7d40a4e3958cdef7d4375
Charles-MAD <charlesdang00@gmail.com>
2024-01-17 00:33:41 +0200

    Ajout de comentaires

--- alphabetDonne.js
+++ alphabetDonne.js
@@ -2,12 +2,12 @@ function add(a, b) {
     return a + b;
 }
 let result = add(2,3);
-console.log(result);
+console.log(result); // affiche 5
 
- console.log('');
- 
+ console.log(''); // affiche un espace entre les deux resultats
+
 function mult(c, d) {
     return c * d;
 }
-let resultat = mult(2,3);
+let resultat = mult(2,3); // affiche 6
 console.log(resultat);
